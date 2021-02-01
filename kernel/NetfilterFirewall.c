d46c693c65851fefa3adc7b33fea0f4b613fbdb7
Alice-ling <64893764+Alice-ling@users.noreply.github.com>
2021-02-01 20:25:00 +0800

    Update NetfilterFirewall.c

--- kernel/NetfilterFirewall.c
+++ kernel/NetfilterFirewall.c
@@ -36,7 +36,7 @@
 #include <linux/netfilter_bridge.h>
 
 // const variables  define
-#define CDEV_NAME "fpNetfilterFirewall"
+#define CDEV_NAME "NetfilterFirewall"
 #define CLASS_NAME "fp"
 #ifndef __FW_INCLUDE__
 #define __FW_INCLUDE__
